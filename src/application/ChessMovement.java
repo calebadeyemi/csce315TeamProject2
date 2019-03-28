0ee82d35f8f2ff0899fa264077a587ea6bec1787
Sausha-RaeBrown <sausha_rae@tamu.edu>
2019-03-27 19:31:08 -0500

    Add Queen Movement

--- src/application/ChessMovement.java
+++ src/application/ChessMovement.java
@@ -296,7 +296,189 @@ class ChessMovement {
     }
 
     private static ArrayList<Position> queenMoves(int[][] gameState, int col, int row) {
-        return new ArrayList<>();
+
+        ArrayList<Position> moves = new ArrayList<>();
+
+        //get the value of the present piece to determine set
+        int pieceColor = gameState[row][col];
+
+        int i = 0;
+        int num = 0;
+
+        //queen can move diagonally to the left
+        int[] rowOffset = {-1};
+        int[] colOffset = {-1};
+        while(num < 8) {
+            for (int side : rowOffset) {
+                for (int side2 : colOffset) {
+                    int tgtRow = side + row + i;
+                    int tgtCol = side2 + col + i;
+
+                    if (inBounds(tgtRow, tgtCol, 8)) {
+                        //check to see when can attack ( same color)
+
+                        Position p = new Position(tgtCol, tgtRow);
+                        moves.add(p);
+                    }
+                }
+            }
+            i = i -1;
+            num++;
+        }
+
+        //queen can move diagonally to the right
+        num = 0;
+        int j =0;
+        int[] rowOffset2 = {-1};
+        int[] colOffset2 = {1};
+
+        while(num < 8) {
+            for (int side3 : rowOffset2) {
+                for (int side4 : colOffset2) {
+                    int tgtRow2 = side3 + row + j;
+                    int tgtCol2 = side4 + col - j;
+
+                    if (inBounds(tgtRow2, tgtCol2, 8)) {
+                        //check to see when can attack ( same color)
+
+                        Position p = new Position(tgtCol2, tgtRow2);
+                        moves.add(p);
+                    }
+                }
+            }
+            j = j -1;
+            num++;
+        }
+
+        //queen can move down diagonally to the left
+        num = 0;
+        int k =0;
+        int[] rowOffset3 = {1};
+        int[] colOffset3 = {1};
+        while(num < 8) {
+            for (int side5 : rowOffset3) {
+                for (int side6 : colOffset3) {
+                    int tgtRow3 = side5 + row + k;
+                    int tgtCol3 = side6 + col + k;
+
+                    if (inBounds(tgtRow3, tgtCol3, 8)) {
+                        //check to see when can attack ( same color)
+
+                        Position p = new Position(tgtCol3, tgtRow3);
+                        moves.add(p);
+                    }
+                }
+            }
+            k = k + 1;
+            num++;
+        }
+
+        //queen can move down diagonally to the right
+        num = 0;
+        int l =0;
+        int[] rowOffset4 = {-1};
+        int[] colOffset4 = {1};
+
+        while(num < 8) {
+            for (int side7 : rowOffset4) {
+                for (int side8 : colOffset4) {
+                    int tgtRow4 = side7 + row - l;
+                    int tgtCol4 = side8 + col + l;
+
+                    if (inBounds(tgtRow4, tgtCol4, 8)) {
+                        //check to see when can attack ( same color)
+
+                        Position p = new Position(tgtCol4, tgtRow4);
+                        moves.add(p);
+                    }
+                }
+            }
+            l = l -1;
+            num++;
+        }
+
+////////////////////////////////////////////////////////////
+
+        int[] rowOffset5 = {1};
+        int[] colOffset5 = {1};
+
+        //queen can move up
+        int r = 0;
+        num = 0;
+        while(num < 8){
+            for(int side : rowOffset5) {
+                int tgtRow5 = row + r;
+
+                if (inBounds(tgtRow5, col, 8)) {
+                    //check to see when can attack ( same color)
+
+                    Position p = new Position(col, tgtRow5);
+                    moves.add(p);
+                }
+
+            }
+            r = r -1;
+            num++;
+        }
+
+        //queen can move left
+        num = 0;
+        int s = 0;
+        while(num < 8){
+            for(int side2 : colOffset5) {
+                int tgtCol5 = col - s;
+
+                if (inBounds(row, tgtCol5, 8)) {
+                    //check to see when can attack ( same color)
+
+                    Position p = new Position(tgtCol5, row);
+                    moves.add(p);
+                }
+
+            }
+            s = s + 1;
+            num++;
+        }
+
+        //queen can move down
+        num = 0;
+        int m = 0;
+        while(num < 8){
+            for(int side3 : rowOffset5) {
+                int tgtRow6 = row + m;
+
+                if (inBounds(tgtRow6, col, 8)) {
+                    //check to see when can attack ( same color)
+
+                    Position p = new Position(col, tgtRow6);
+                    moves.add(p);
+                }
+
+            }
+            m = m + 1;
+            num++;
+        }
+
+        //queen can move right
+        num = 0;
+        int n = 0;
+        while(num < 8){
+            for(int side4 : colOffset5) {
+                int tgtCol7 = col + n;
+
+                if (inBounds(row, tgtCol7, 8)) {
+                    //check to see when can attack ( same color)
+
+                    Position p = new Position(tgtCol7, row);
+                    moves.add(p);
+                }
+
+            }
+            n = n + 1;
+            num++;
+        }
+ ////////////////////////////////////////////////////////////////////
+        return moves;
     }
 
     private static ArrayList<Position> kingMoves(int[][] gameState, int col, int row) {
